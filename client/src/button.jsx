e703ca923eb7a13e29df964d68886a413450f116
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-10-17 13:22:35 +0530

    fgfgh

--- client/src/button.jsx
+++ client/src/button.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import './button.css';  // Import the CSS file
 
-const Button = ({isOn,handleSwitchChange}) => {
+const Button = ({ isOn, handleSwitchChange }) => {
 
-  return (
-    <div className="box">
-      <label>
-        <input type="checkbox" checked={isOn} onChange={handleSwitchChange} />
-        <span>
-          <i className={`fa fa-power-off ${isOn ? 'on' : ''}`} aria-hidden="true"></i>
-        </span>
-      </label>
-      <div id="led" style={{ display: isOn ? 'block' : 'none' }}>hello</div>
-    </div>
-  );
+    return (
+        <>
+            <div className="box">
+                <label>
+                    <input type="checkbox" checked={isOn} onChange={handleSwitchChange} />
+                    <span>
+                        <i className={`fa fa-power-off ${isOn ? 'on' : ''}`} aria-hidden="true"></i>
+                    </span>
+                </label>
+            </div>
+            <div id="led" > {isOn ? "ON":"OFF"}</div>
+        </>
+    );
 };
 
 export default Button;
