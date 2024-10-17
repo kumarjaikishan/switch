5e20bf09a6306bf0ed6a574a3da5aaaaab82d453
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-10-17 13:10:05 +0530

    dfg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,14 @@
 
 import React, { useState, useEffect } from 'react';
 import socketIOClient from 'socket.io-client';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
 import './App.css'
-import { styled } from '@mui/material/styles';
+import Button from './button';
 
 const endpoint = '/'; // Use the full URL here
 
-
 function App() {
-  const [switchStatus, setSwitchStatus] = useState(false);
-  const [socket, setSocket] = useState(null); // Create state for socket
+  const [isOn, setIsOn] = useState(false);
+  const [socket, setSocket] = useState(null);
 
   useEffect(() => {
     console.log(endpoint)
@@ -19,78 +16,26 @@ function App() {
     setSocket(socketIo); // Save socket connection in state
 
     socketIo.on('initialSwitchStatus', ({ status }) => {
-      setSwitchStatus(status);
-      console.log('initial status:',status)
+      setIsOn(status)
+      console.log('initial status:', status)
     });
     socketIo.on('switchStatusChanged', ({ status }) => {
-      setSwitchStatus(status);
-      // console.log(status);
+      setIsOn(status)
     });
     return () => socketIo.disconnect();
-  }, []); // Empty dependency array ensures this runs only on mount and unmount
+  }, []);
 
   const handleSwitchChange = () => {
-    const newStatus = !switchStatus;
-    setSwitchStatus(newStatus);
+    const newStatus = !isOn;
+    setIsOn(!isOn)
     if (socket) {
       socket.emit('socketstatus', newStatus); // Now socket is in scope
     }
   };
 
-  const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-    width: 62,
-    height: 34,
-    padding: 7,
-    '& .MuiSwitch-switchBase': {
-      margin: 1,
-      padding: 0,
-      transform: 'translateX(6px)',
-      '&.Mui-checked': {
-        color: '#fff',
-        transform: 'translateX(22px)',
-        '& .MuiSwitch-thumb:before': {
-          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-            '#fff',
-          )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-        },
-        '& + .MuiSwitch-track': {
-          opacity: 1,
-          backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-        },
-      },
-    },
-    '& .MuiSwitch-thumb': {
-      backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
-      width: 32,
-      height: 32,
-      '&::before': {
-        content: "''",
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        left: 0,
-        top: 0,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          '#fff',
-        )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-      },
-    },
-    '& .MuiSwitch-track': {
-      opacity: 1,
-      backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-      borderRadius: 20 / 2,
-    },
-  }));
-
   return (
     <div>
-      <p>Switch is {switchStatus ? 'on' : 'off'}</p> {/* Display switch state */}
-      <FormControlLabel
-        control={<MaterialUISwitch onChange={handleSwitchChange} checked={switchStatus} sx={{ m: 1 }}  />}
-        label="MUI switch"
-      />
+      <Button isOn={isOn} setIsOn={setIsOn} handleSwitchChange={handleSwitchChange} />
     </div>
   );
 }
